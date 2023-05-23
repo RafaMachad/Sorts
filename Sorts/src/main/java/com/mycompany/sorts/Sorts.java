72d411bace8d76a608bba98a9c5d9b59b7419e59
RafaMachad <rmr.eu@hotmail.com.br>
2023-05-23 13:03:12 -0300

    Primeiro Commit
    +Interface
    +Design

---
+++ Sorts/src/main/java/com/mycompany/sorts/Sorts.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sorts;
+
+
+public class Sorts {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
